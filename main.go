3db59b384e85181a4d872fca0834fced8ed03bf5
Evgeniy Khaluev <eugene.haluev@gmail.com>
2020-11-11 17:22:32 +0700

    Add XTA ATX instructions

--- main.go
+++ main.go
@@ -5,7 +5,112 @@ import (
 	"log"
 )
 
+// Short address opcodes (BIT20 = 0)
+const (
+	OpATX = iota
+	OpSTX
+	OpMOD
+	OpXTS
+	OpADD
+	OpSUB
+	OpRSUB
+	OpAMX
+	OpXTA
+	OpAAX
+	OpAEX
+	OpARX
+	OpAVX
+	OpAOX
+	OpDIV
+	OpMUL
+	OpAPX
+	OpAUX
+	OpACX
+	OpANX
+	OpEADDX
+	OpESUBX
+	OpASX
+	OpXTR
+	OpRTE
+	OpYTA
+	OpE32
+	OpE33
+	OpEADDN
+	OpESUB
+	OpASN
+	OpNTR
+	OpATI
+	OpSTI
+	OpITA
+	OpITS
+	OpMTJ
+	OpJADDM
+	OpE46
+	OpE47
+	OpE50
+	OpE51
+	OpE52
+	OpE53
+	OpE54
+	OpE55
+	OpE56
+	OpE57
+	OpE60
+	OpE61
+	OpE62
+	OpE63
+	OpE64
+	OpE65
+	OpE66
+	OpE67
+	OpE70
+	OpE71
+	OpE72
+	OpE73
+	OpE74
+	OpE75
+	OpE76
+	OpE77
+)
+
+// Long address opcodes (BIT20 = 1)
+const (
+	OpE20 = 0o200 + iota*0o10
+	OpE21
+	OpUTC
+	OpWTC
+	OpVTM
+	OpUTM
+	OpUZA
+	OpUIA
+	OpUJ
+	OpVJM
+	OpIJ
+	OpSTOP
+	OpVZM
+	OpVIM
+	OpE36
+	OpVLM
+)
+
 type besmWord uint64
+
+func emitOp(ind uint16, op uint16, addr uint16) (word besmWord, err error) {
+	word = besmWord(ind&0xF) << 20
+	addr = addr & 0o77777
+	if op <= OpE77 {
+		if addr >= 0o7777 && addr <= 0o67777 {
+			return 0, fmt.Errorf("Address: %05o out of range for `short` command", addr)
+		}
+		word |= (besmWord(op) << 12) + (besmWord(addr) & 0o7777)
+		if addr >= 0o70000 {
+			word |= 0o1000000 // set address extension BIT19 = 1
+		}
+	} else {
+		word |= (besmWord(op) << 12) + (besmWord(addr) & 0o77777)
+	}
+	return word, nil
+}
 
 type MemRegion struct {
 	start uint16
@@ -103,16 +208,26 @@ func (bus *Bus) write(addr uint16, value besmWord) {
 
 // CPU state
 type CPU struct {
-	PC      uint16     // instruction pointer
-	ACC     besmWord   // accumulator
-	M       [16]uint16 // M registers (index registers, address modifiers)
-	ibus    *Bus       // instruction bus
-	dbus    *Bus       // data bus
-	right   bool       // right or left instruction flag
-	irCache besmWord   // instruction cache register
-	ir      besmWord   // current being executed instruction
-	stack   bool       // current instruction in stack mode
-	uaddr   uint16     // u_addr = i_addr + m[ind] + c_addr (if set)
+	PC        uint16     // instruction pointer
+	ACC       besmWord   // accumulator
+	M         [16]uint16 // M registers (index registers, address modifiers)
+	ibus      *Bus       // instruction bus
+	dbus      *Bus       // data bus
+	right     bool       // right or left instruction flag
+	irCache   besmWord   // instruction cache register
+	ir        besmWord   // current being executed instruction
+	stack     bool       // current instruction in stack mode
+	isRunning bool
+
+	irOP   uint16
+	irIND  uint16
+	irAddr uint16
+
+	cActive bool
+	cReg    uint16
+	vAddr   uint16 // ir_addr + c_mod  if set c_active (OpUTC OpWTC)
+
+	rrReg uint16 // machine mode and flag register
 }
 
 func (cpu *CPU) reset() {
@@ -120,13 +235,38 @@ func (cpu *CPU) reset() {
 	cpu.ACC = 0
 	cpu.M = [16]uint16{0}
 	cpu.right = false
+	cpu.isRunning = false
+}
+
+func (cpu *CPU) setRLog() {
+	cpu.rrReg = cpu.rrReg&0b11100011 | 0b0000100
+}
+
+func (cpu *CPU) setRMul() {
+	cpu.rrReg = cpu.rrReg&0b11100011 | 0b0001000
+}
+
+func (cpu *CPU) setRAdd() {
+	cpu.rrReg = cpu.rrReg&0b11100011 | 0b0010000
 }
 
 func (cpu *CPU) atx() {
-	cpu.dbus.write(cpu.uaddr, cpu.ACC)
+	cpu.dbus.write(cpu.uAddr(), cpu.ACC)
 	if cpu.stack {
-		cpu.M[15]++
+		cpu.M[15] = (cpu.M[15] + 1) & 0o77777
 	}
+}
+
+func (cpu *CPU) xta() {
+	if cpu.stack {
+		cpu.M[15] = (cpu.M[15] - 1) & 0o77777
+	}
+	cpu.ACC = cpu.dbus.read(cpu.uAddr())
+	cpu.setRLog()
+}
+
+func (cpu *CPU) uAddr() uint16 {
+	return (cpu.M[cpu.irIND] + cpu.vAddr) & 0o77777
 }
 
 func (cpu *CPU) step() {
@@ -142,122 +282,61 @@ func (cpu *CPU) step() {
 		pcNext = (cpu.PC + 1) & 0o77777
 	}
 	cpu.right = !cpu.right
-	// DECODE instruction
-	//ind := (cpu.ir >> 20) & 0xF
-	//fmt.Println("ind:", ind)
+	// DECODE step 1. unpack instruction
+	cpu.irIND = uint16((cpu.ir >> 20) & 0xF)
+	if cpu.ir&0o2000000 == 0 {
+		cpu.irOP = uint16((cpu.ir & 0o770000) >> 12)
+		cpu.irAddr = uint16(cpu.ir & 0o7777)
+		if cpu.ir&0o2000000 != 0 {
+			cpu.irAddr |= 0o70000
+		}
+	} else {
+		cpu.irOP = uint16((cpu.ir&0o1700000)>>12 + 0o200)
+		cpu.irAddr = uint16(cpu.ir & 0o77777)
+	}
+	// DECODE step 2. modify execution address if needed
+	if cpu.cActive {
+		cpu.vAddr = (cpu.irAddr + cpu.cReg) & 0o77777
+	} else {
+		cpu.vAddr = cpu.irAddr
+	}
+	cpu.cActive = false
+	// DECODE step 3. set stack mode flag
+	cpu.stack = false
+	if cpu.irIND == 15 {
+		if cpu.vAddr == 0 {
+			cpu.stack = true
+		} else {
+			if cpu.irOP == OpSTI && cpu.uAddr() == 15 {
+				cpu.stack = true
+			}
+		}
+	}
+	// EXECUTE
+	switch cpu.irOP {
+	case OpATX:
+		cpu.atx()
+	case OpXTA:
+		cpu.xta()
+	default:
+		log.Printf("Unimplemented opcode: %03o", cpu.irOP)
+		cpu.isRunning = false
+	}
 	// advance instrunction pointer
 	cpu.PC = pcNext
 }
 
 func (cpu *CPU) state() {
-	fmt.Printf("PC:\t%05o right:%t IR:%016o\n", cpu.PC, cpu.right, cpu.ir)
-	fmt.Printf("M:\t%05v\n", cpu.M)
+	fmt.Printf("PC:\t%05o right: %t IR: %08o\n", cpu.PC, cpu.right, cpu.ir)
+	fmt.Printf("M:\t%05o\n", cpu.M)
 	fmt.Printf("ACC:\t%016o\n", cpu.ACC)
 }
 
-// Short address opcodes (BIT20 = 0)
-const (
-	OpATX = iota
-	OpSTX
-	OpMOD
-	OpXTS
-	OpADD
-	OpSUB
-	OpRSUB
-	OpAMX
-	OpXTA
-	OpAAX
-	OpAEX
-	OpARX
-	OpAVX
-	OpAOX
-	OpDIV
-	OpMUL
-	OpAPX
-	OpAUX
-	OpACX
-	OpANX
-	OpEADDX
-	OpESUBX
-	OpASX
-	OpXTR
-	OpRTE
-	OpYTA
-	OpE32
-	OpE33
-	OpEADDN
-	OpESUB
-	OpASN
-	OpNTR
-	OpATI
-	OpSTI
-	OpITA
-	OpITS
-	OpMTJ
-	OpJADDM
-	OpE46
-	OpE47
-	OpE50
-	OpE51
-	OpE52
-	OpE53
-	OpE54
-	OpE55
-	OpE56
-	OpE57
-	OpE60
-	OpE61
-	OpE62
-	OpE63
-	OpE64
-	OpE65
-	OpE66
-	OpE67
-	OpE70
-	OpE71
-	OpE72
-	OpE73
-	OpE74
-	OpE75
-	OpE76
-	OpE77
-)
-
-// Long address opcodes (BIT20 = 1)
-const (
-	OpE20 = 0o200 + iota*0o10
-	OpE21
-	OpUTC
-	OpWTC
-	OpVTM
-	OpUTM
-	OpUZA
-	OpUIA
-	OpUJ
-	OpVJM
-	OpIJ
-	OpSTOP
-	OpVZM
-	OpVIM
-	OpE36
-	OpVLM
-)
-
-func emitOp(ind uint16, op uint16, addr uint16) (word besmWord, err error) {
-	word = besmWord(ind&0xF) << 20
-	addr = addr & 0o77777
-	if op <= OpE77 {
-		if addr >= 0o7777 && addr <= 0o67777 {
-			return 0, fmt.Errorf("Address: %05o out of range for `short` command", addr)
-		}
-		word |= (besmWord(op) << 12) + (besmWord(addr) & 0o7777)
-		if addr >= 0o70000 {
-			word |= 0o1000000
-		}
-	} else {
-		word |= (besmWord(op) << 12) + (besmWord(addr) & 0o77777)
+func (cpu *CPU) run() {
+	cpu.isRunning = true
+	for cpu.isRunning {
+		cpu.step()
 	}
-	return word, nil
 }
 
 func newCPU(ibus *Bus, dbus *Bus) *CPU {
@@ -274,10 +353,19 @@ func main() {
 
 	ibus := newBus("IBUS")
 	dbus := newBus("DBUS")
-	ibus.attach(MemRegion{0, 2047}, &rom)
+	ibus.attach(MemRegion{0, 1023}, &rom)
 	dbus.attach(MemRegion{0, 1023}, &ram)
 
-	// cpu := newCPU(ibus, dbus)
-
-	fmt.Printf("%0o\n", OpE77)
+	cpu := newCPU(ibus, dbus)
+	cpu.ACC = 0o10
+	cpu.M[15] = 0o55
+	left, _ := emitOp(15, OpATX, 0)
+	right, _ := emitOp(15, OpXTA, 0)
+	ibus.write(1, left<<24+right)
+	cpu.state()
+	cpu.step()
+	cpu.ACC = 0
+	cpu.state()
+	cpu.step()
+	cpu.state()
 }
--- main_test.go
+++ main_test.go
@@ -86,12 +86,16 @@ func TestPCIncrement(t *testing.T) {
 func TestCPUReset(t *testing.T) {
 	ibus := newBus("IBUS")
 	cpu := newCPU(ibus, ibus)
+	cpu.M[4] = 4
 	cpu.reset()
 	if cpu.PC != 1 {
 		t.Error("PC is not equal to 1")
 	}
 	if cpu.right != false {
 		t.Error("Execution need to start from left instruction")
+	}
+	if cpu.M[4] != 0 {
+		t.Error("CPU M registers no cleared")
 	}
 }
 
@@ -117,5 +121,53 @@ func TestEmitOp(t *testing.T) {
 	w, e = emitOp(8, OpSTOP, 0o54321)
 	if e == nil && w != 0o43354321 {
 		t.Errorf("STOP 54321(10) emit error. W: %08o %024b", w, w)
+	}
+}
+
+func TestATXXTA(t *testing.T) {
+	mem := newMemory("MEM", 1024)
+	ibus := newBus("IBUS")
+	ibus.attach(MemRegion{0, 1023}, &mem)
+	cpu := newCPU(ibus, ibus)
+	cpu.ACC = 0xC0DE
+	// normal mode
+	instr, _ := emitOp(0, OpATX, 55)
+	mem.write(1, instr<<24)
+	cpu.step()
+	if mem.read(55) != 0xC0DE {
+		t.Error("normal mode ATX is not working")
+	}
+	// stack mode
+	cpu.reset()
+	cpu.M[15] = 15
+	cpu.ACC = 12345
+	instr, _ = emitOp(15, OpATX, 0)
+	mem.write(1, instr<<24)
+	cpu.step()
+	if mem.read(15) != 12345 {
+		t.Error("Stack mode ATX is not working")
+	}
+	// test xta
+	cpu.reset()
+	instr, _ = emitOp(0, OpXTA, 55)
+	mem.write(1, instr<<24)
+	cpu.step()
+	if cpu.ACC != 0xC0DE {
+		t.Error("Normal mode XTA not mowrking")
+	}
+	if cpu.rrReg&4 == 0 {
+		t.Error("Logial mode flag is not set")
+	}
+	// test stack mode xta
+	cpu.reset()
+	cpu.M[15] = 56
+	instr, _ = emitOp(15, OpXTA, 0)
+	mem.write(1, instr<<24)
+	cpu.step()
+	if cpu.ACC != 0xC0DE {
+		t.Error("Stack mode XTA not mowrking")
+	}
+	if cpu.rrReg&4 == 0 {
+		t.Error("Logial mode flag is not set")
 	}
 }
